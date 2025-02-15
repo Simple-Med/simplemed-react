901c63263535d1486459ae82aafbf42e05cccbca
Felipe Lohan <felipelohan03052006@gmail.com>
2025-02-15 15:56:36 -0300

    Faq Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
 import { HeroBanner } from "./components/HeroBanner";
 import { FormTest } from "./components/FormTest";
+import { Faq } from "./components/Faq";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <HeroBanner />
       <FormTest />
+      <Faq />
     </>
   );
 }
